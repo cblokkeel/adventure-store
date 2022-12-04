99d537a37882c6adb4a60ebcac2a3a9280c187f5
cblokkeel <cblokkeel@partner.auchan.fr>
2022-12-04 12:03:51 +0100

    fetch products and display in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">All products</h2>
+
+    <div class="grid grid-cols-2 gap-7">
+      <div v-for="product in products" :key="product.id">
+        <!-- <ProductCard :product="product" /> -->
+        {{ product.title }}
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Product } from '~/lib/types';
+
+const { data: products } = await useFetch<Product[]>(
+  'http://localhost:4000/products',
+);
+</script>
 
 <style scoped></style>
